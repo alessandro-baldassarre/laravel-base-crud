bc0a4c10d4c656f424fe81caa913ada6f266edac
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-05-17 15:36:29 +0200

    execute ComicSeeder with comics.php(data)

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Comic;
 class ComicSeeder extends Seeder
 {
     /**
@@ -11,6 +11,20 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
+        include('config/comics.php');
 
+        foreach ($comics as $key => $comic) {
+
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+
+            $newComic->save();
+
+        }
     }
 }
